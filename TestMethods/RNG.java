1b6537fc8bc726d9cb6ab7b396df428e40e776a4
MelkIsLekker <gerhardmaritz@proton.me>
2025-04-15 09:43:17 +0200

    updates

--- TestMethods/RNG.java
+++ TestMethods/RNG.java
@@ -5,13 +5,15 @@ import java.util.Random;
 public class RNG {
  public static void main(String[] args) {
 
+    final int size = 10_000_000;
+
     Random generator = new Random();
     int i = 0;
     try {
         FileWriter fw = new FileWriter("random_number2.txt");
 
-        while (i < 10_000_000) {
-            fw.write(generator.nextInt(10_000_000) +" ");
+        while (i < size) {
+            fw.write(generator.nextInt(size) +" ");
             i++;
         }
     } catch (IOException e) {
